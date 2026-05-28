c6a7347e3b399643b5284b19ecb9ee4e120d8957
Hiếu GL Lite <100750590+ShilukaYT@users.noreply.github.com>
2026-05-28 13:28:46 +0700

    Update ghRawURL for tiktok-rss-flat repository

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Set your GH Pages URL here e.g.:
 ghPagesURL = "https://shilukayt.github.io/tiktok-rss-flat/"
-ghRawURL = "https://raw.githubusercontent.com/ShilukaYT/HieuGLLiteFE/main/"
+ghRawURL = "https://raw.githubusercontent.com/ShilukaYT/tiktok-rss-flat/main/"
 
 # ghPagesURL = "https://tiktokrss.conoroneill.com/"
