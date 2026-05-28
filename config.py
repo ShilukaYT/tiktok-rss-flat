5b9746254b1b1369d5f45272ca6abb26f39a29e8
Hiếu GL Lite <100750590+ShilukaYT@users.noreply.github.com>
2026-05-28 13:06:53 +0700

    Update ghPagesURL to new TikTok RSS link

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Set your GH Pages URL here e.g.:
-ghPagesURL = "https://shilukayt.github.io/HieuGLLiteFE/"
+ghPagesURL = "https://shilukayt.github.io/tiktok-rss-flat/"
 ghRawURL = "https://raw.githubusercontent.com/ShilukaYT/HieuGLLiteFE/main/"
 
 # ghPagesURL = "https://tiktokrss.conoroneill.com/"
